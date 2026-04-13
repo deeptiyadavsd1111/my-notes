94ab7228cba781ff62a93055ed4ebe3537190263
Deepti Yadav <deeptiyadavsd1111@gmail.com>
2026-04-14 02:05:13 +0530

    Change site title from 'My Notes' to 'DY Notes'

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Notes',
+  title: 'DY Notes',
   tagline: 'My personal knowledge base',
   favicon: 'img/favicon.ico',
 
